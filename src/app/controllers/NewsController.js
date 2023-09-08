e9e42e6a4c03de40ce2e9aea1b78661deebf556d
ChungEnzo <chungche8888@gmail.com>
2023-09-08 14:20:06 +0900

    Route

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,6 +1,4 @@
-
 class NewsController {
-
     // GET /news
     index(req, res) {
         res.render('news');
@@ -9,8 +7,7 @@ class NewsController {
     // GET /news/:slug
     show(req, res) {
         res.send('abc');
-    } 
+    }
 }
-
 
 module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
-const route = require("./routes");
+const route = require('./routes');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine setup
-app.engine("hbs", handlebars({
-  extname: ".hbs"
-}));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () =>
-  console.log(`Example app listening on http://localhost:${port}`)
+    console.log(`Example app listening on http://localhost:${port}`),
 );
