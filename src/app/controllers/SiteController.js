aaeeb2cf971543cd0f68ee4fe2c970fae25e3db0
ChungEnzo <chungche8888@gmail.com>
2023-09-08 17:37:46 +0900

    update code

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,16 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // GET /
-    async index(req, res) {
-        try {
-            const courses = await Course.find({});
-            res.json(courses);
-        } catch (err) {
-            res.status(500).json({ error: 'ERROR!!!' });
-        }
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // GET /search
